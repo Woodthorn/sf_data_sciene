0da2056b1b60f02330498fd4db5548fa1e96af47
Woodthorn <wolfthornn@gmail.com>
2023-03-03 18:06:54 +0300

    edit

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 
-def random_predict(number: int = 1) -> int:
+def game_core_v3(number: int = 1) -> int:
     """
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -27,11 +27,11 @@ def random_predict(number: int = 1) -> int:
             
     return count
 
-def score_game(random_predict) -> int:
+def score_game(game_core_v3) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        game_core_v3 ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
@@ -41,7 +41,7 @@ def score_game(random_predict) -> int:
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(game_core_v3(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
@@ -50,4 +50,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(game_core_v3)
