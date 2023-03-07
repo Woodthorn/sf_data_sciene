3475d1f4989b4c9862da9bdc2278f24486f9fb89
Woodthorn <wolfthornn@gmail.com>
2023-03-07 14:25:47 +0300

    edit

--- project_0/game.py
+++ project_0/game.py
@@ -9,14 +9,14 @@ count = 0
 
 while True:
     count += 1
-    predict_number = int(input("Угадай число от 1 до 100: "))
+    predict_number = int(input('Угадай число от 1 до 100: '))
 
     if predict_number > number:
-        print("Число должно быть меньше!")
+        print('Число должно быть меньше!')
 
     elif predict_number < number:
-        print("Число должно быть больше!")
+        print('Число должно быть больше!')
 
     else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        print(f'Вы угадали число! Это число = {number}, за {count} попыток')
         break  # конец игры выход из цикла
